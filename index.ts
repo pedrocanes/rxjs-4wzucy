65e0677b434a49cba972cb47133cae2e721d8a75
pedrocanes <pedro.canes2@mail.dcu.ie>
2020-09-11 16:49:03 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,16 +1,19 @@
 import { of, Observable } from 'rxjs'; 
-import { map } from 'rxjs/operators';
+import { map, timeout } from 'rxjs/operators';
 
 
 // const source = of('World').pipe(
 //   map(x => `Hello ${x}!`)
 // );
 
-// source.subscribe(x => console.log(x));
+// source.subscribe(x => console.log(x))
+
 
 const hello = Observable.create(function(observer) {
   observer.next(42);
+  observer.next(43);
+  observer.next(44);
   observer.complete();
 });
 
-const subscribe = hello.subscribe(val => console.log(val));
+const subscribe = hello.pipe(timeout(10)).subscribe(val => console.log(val));
