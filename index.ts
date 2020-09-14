319e7cdd903779d9e4934ce2e16ac465fa8d64b3
pedrocanes <pedro.canes2@mail.dcu.ie>
2020-09-14 16:53:32 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,23 +1,33 @@
-import { of, Observable } from 'rxjs'; 
+import { of, Observable, observable, Subject, combineLatest } from 'rxjs'; 
 import { map, timeout } from 'rxjs/operators';
 
-
-// const source = of('World').pipe(
-//   map(x => `Hello ${x}!`)
-// );
-
-// source.subscribe(x => console.log(x))
-
-
-const hello = Observable.create(function(observer) {
-  setTimeout(() => {
-    observer.next(42);
-    observer.next(43);
-    observer.next(44);
-    observer.complete();
-  }, 1000);
+const subject = new Subject();
+const subject2 = new Subject();
+ 
+subject.subscribe({
+  next: (v) => console.log(v)
 });
 
-const subscribe = hello
-  //.pipe(timeout(10))
-  .subscribe(val => console.log(val));
+subject2.subscribe({
+  next: (v) => console.log(v)
+});
+ 
+subject.next(1);
+
+subject2.next('A');
+
+subject.next(2);
+
+subject2.next('B');
+subject2.next('C');
+subject2.next('D');
+
+subject.next(3);
+subject.next(4);
+subject.next(5);
+
+subject.complete();
+subject2.complete();
+
+const combinedTimers = combineLatest(subject, subject2);
+combinedTimers.subscribe(value => console.log(value));
