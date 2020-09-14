90bc8e30e9e9f5ebb087a2c524f3e421a1677e79
pedrocanes <pedro.canes2@mail.dcu.ie>
2020-09-14 09:01:53 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -9,7 +9,6 @@ import { map, timeout } from 'rxjs/operators';
 // source.subscribe(x => console.log(x))
 
 
-//emits values of any type
 const source = of(42,43,45);
-//output: {name: 'Brian'}, [1,2,3], function hello() { return 'Hello' }
+
 const subscribe = source.subscribe(val => console.log(val));
