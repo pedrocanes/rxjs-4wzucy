050e161fcce8c2b2f8f3fe8d6d0209f4a2ce48fd
pedrocanes <pedro.canes2@mail.dcu.ie>
2020-09-15 14:56:51 +0100

    2 files modified

--- index.ts
+++ index.ts
@@ -1,36 +1,11 @@
 import { fromEvent, interval, merge, observable, from } from 'rxjs';
-import { debounce, mapTo, filter, map, switchMap, debounceTime, buffer, delay } from 'rxjs/operators';
+import { debounce, mapTo, filter, map, switchMap, debounceTime, buffer, delay, throttle } from 'rxjs/operators';
 
+const redDiv = document.getElementById('red');
+const blueDiv = document.getElementById('blue');
 
+const redEvent = fromEvent(redDiv, 'mouseover');
+const blueEvent = fromEvent(blueDiv, 'mouseover');
 
-var checkBoxes = document.querySelectorAll('input');
-const example = fromEvent(checkBoxes, 'change');
-
-const result = from(fetch('https://jsonplaceholder.typicode.com/todos').then(res => res.json()));
-
-// example.pipe(
-//   debounceTime(1000),
-//   filter(e => e.target.checked))
-//   .subscribe(event => {
-//     result.pipe(
-//       map((val) => {
-//         return val.filter(num => num.userId == event.target.id);
-//       })
-//     ).subscribe((val) => {
-//       console.log(val.map((comment) => comment.title))
-//     })
-//   });
-
-example.pipe(
-  delay(1000),
-  filter(e => e.target.checked),
-  switchMap(event => {
-    return result.pipe(
-      map((val) => {
-        return val.filter(num => num.userId == event.target.id);
-      })
-    )
-  })
-).subscribe((val) => {
-      console.log(val.map((comment) => comment.title))
-    })
+const colorObs = merge(blueEvent.pipe(mapTo("blue")), redEvent.pipe(mapTo("red"))).pipe(throttle(ev => interval(1000)));
+colorObs.subscribe((val) => console.log(val));
